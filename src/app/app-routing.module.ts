6422893a246333bfd2b3c1704ba39a536950fe00
suryakumari656 <sailakshmikothapalli1997@gmail.com>
2020-04-01 19:51:11 +0530

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { LoansComponent } from './loans/loans.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'header',component:HeaderComponent},
+  {path:'home',component:HomeComponent},
+  {path:'loans',component:LoansComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { LoansComponent } from './loans/loans.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoansComponent
   ],
   imports: [
     BrowserModule,
